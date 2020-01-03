42ff6dcf638deb06eb780b56a7ea7f4307f0ebc2
Youngjin Kwak <46660361+kkan0615@users.noreply.github.com>
2020-01-03 00:24:51 -0500

    Update

---
+++ vue-second-hand-nuxt/layouts/default.vue
@@ -0,0 +1,88 @@
+<!--
+    Author: Youngjin Kwak
+    Start: 10/25/2019
+    Update: 01/02/2020
+    Purpose: default layout
+ -->
+<template>
+    <v-app>
+        <Drawer :drawer="drawer"></Drawer>
+        <Bar @changeDrawer="fromBar"></Bar>
+        <v-content
+        app
+        >
+            <v-container
+            >
+                <nuxt />
+            </v-container>
+        </v-content>
+    </v-sheet>
+    <Footer />
+    </v-app>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue'
+import Bar from '../components/Bar.vue'
+import Drawer from '../components/Drawer.vue'
+
+export default {
+    data: () => ({
+        collapseOnScroll: true,
+        drawer: false,
+    }),
+    components: {
+        Footer,
+        Bar,
+        Drawer,
+    },
+    head: {
+        script: [
+            { src: 'https://kit.fontawesome.com/cdc70df69c.js' },
+            { scr: '' }
+        ],
+    },
+    methods: {
+        onLogOut() {
+            this.$store.dispatch('users/logOut')
+            .then((res) => {
+                alert("Success to log out");
+            }).catch((err) => {
+                alert(err);
+            });
+        },
+        fromBar(newDrawer) {
+            this.drawer = newDrawer;
+        }
+    },
+}
+</script>
+<!--
+<style lang="scss" scoped>
+    /*********************************************************************
+    *   content
+    *   Author: Youngjin Kwak
+    *   10/25/2019
+    **********************************************************************/
+    .content {
+        font-family: 'Sunflower', sans-serif;
+        padding: 0px 100px 0px 100px;
+        margin: 0;
+    }
+
+    /*********************************************************************
+    *   footer
+    *   Author: Youngjin Kwak
+    *   10/25/2019
+    **********************************************************************/
+    .footer {
+        clear: both;
+        bottom: 0;
+        padding: 10px;
+        text-align: center;
+        font-size: 20px;
+        background-color: #18D65A;
+        color: white;
+    }
+</style>
+-->
---
+++ vue-second-hand-nuxt/layouts/list_layout.vue
@@ -0,0 +1,89 @@
+<!--
+    Author: Youngjin Kwak
+    Start: 12/20/2019
+    Update: 12/20/2019
+    Purpose: layout for list
+ -->
+<template>
+    <div>
+        <div class="topnav">
+            <nuxt-link to="/">home</nuxt-link>
+            <div class="dropdown" v-if="me">
+                <button @click="onLogOut" v-if="me" >로그아웃</button>
+            </div>
+            <div class="dropdown" v-else>
+                <button class="dropbtn">
+                    로그인하기
+                </button>
+                <div class="dropdown-content">
+                    <nuxt-link to="/login">로그인</nuxt-link>
+                    <nuxt-link to="/signUp">회원가입</nuxt-link>
+                </div>
+            </div>
+            <nuxt-link to="/products">최신순</nuxt-link>
+        </div> <!-- top-nav end -->
+        <div class="content">
+            <nuxt />
+        </div>
+        <div class="footer">
+            @Youngjin kwak
+        </div> <!-- footer end -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        };
+    },
+    head: {
+        script: [
+            { src: 'https://kit.fontawesome.com/cdc70df69c.js' },
+            { scr: '' }
+        ],
+    },
+    computed: {
+        me() {
+            return this.$store.state.users.user;
+        }
+    },
+    methods: {
+        onLogOut() {
+            this.$store.dispatch('users/logOut')
+            .then((res) => {
+                alert("Success to log out");
+            }).catch((err) => {
+                alert(err);
+            });
+        }
+    },
+}
+</script>
+<style lang="scss" scoped>
+    /*********************************************************************
+    *   content
+    *   Author: Youngjin Kwak
+    *   12/20/2019
+    **********************************************************************/
+    .content {
+        font-family: 'Sunflower', sans-serif;
+        padding: 0px 10px 0px 10px;
+        margin: 0;
+    }
+
+    /*********************************************************************
+    *   footer
+    *   Author: Youngjin Kwak
+    *   10/25/2019
+    **********************************************************************/
+    .footer {
+        clear: both;
+        bottom: 0;
+        padding: 10px;
+        text-align: center;
+        font-size: 20px;
+        background-color: #18D65A;
+        color: white;
+    }
+</style>
